5daa15ee003ebb802f9decbdf98d934b7b486cce
vivek.patoriya <vivek.patoriya@42gears.com>
2024-06-06 22:06:12 +0530

    Added contact details in header

---
+++ src/app/service/common.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor() { }
+
+  phoneNumber:string = "+91 7988979889";
+}
