2b3e7d1c8feeb64975baa5af7f1527148af6352c
Rutul sorathiya <rutulsorathiya.14@gmail.com>
2024-06-06 22:07:11 +0530

    feat: add primeng

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CalendarModule } from 'primeng/calendar';
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule, NgFor, NgIf} from "@angular/common";
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CalendarModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  date!:string;
 }
