2170490d32f0c31972661f1fe5137b04ac83c5de
vasu dedakiya <81048950+vasudedakiya@users.noreply.github.com>
2024-06-13 12:29:54 +0000

    autoComplete API intigration.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,8 +6,9 @@ import { DropdownModule } from "primeng/dropdown";
 import { CarouselModule } from 'primeng/carousel';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { VehicleService } from '../../service/vehicle.service';
-import { FirebaseStorage, Storage, ref, uploadBytes, uploadBytesResumable } from '@angular/fire/storage';
-import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
+import { Storage } from '@angular/fire/storage';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { LocationIqService } from '../../service/location-iq.service';
 
 @Component({
@@ -24,55 +25,80 @@ import { LocationIqService } from '../../service/location-iq.service';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   date!: string;
   file!: File;
-  fileName! : string;
+  fileName!: string;
   timeslot: string[] = ['4:00', '5:00'];
   fromText: string | undefined;
-  fromTextUpdate= new Subject<string>();
-  toText:string|undefined;
-  toTextUpdate= new Subject<string>();
-  
+  fromTextUpdate = new Subject<string>();
+  toText: string | undefined;
+  toTextUpdate = new Subject<string>();
+  fromSuggestions: any[] = [];
+  toSuggestions: any[] = [];
+  fromLatLng: { lat: number, lon: number } | undefined;
+  toLatLng: { lat: number, lon: number } | undefined;
+  today: Date = new Date();
+  selectedTime: Date | undefined; 
 
-  vehicleService = inject(VehicleService)
-  private readonly _storage = inject(Storage)
+  vehicleService = inject(VehicleService);
+  private readonly _storage = inject(Storage);
 
-  constructor(private _locationIqService: LocationIqService ){
+  constructor(private _locationIqService: LocationIqService) {
     this.fromTextUpdate.pipe(
-      debounceTime(2000),
-      distinctUntilChanged())
-      .subscribe(value => {
-        console.log("From here",value);
-      });
-
-      this.toTextUpdate.pipe(
-        debounceTime(2000),
-        distinctUntilChanged()).subscribe(value =>{
-          console.log("To here",value)
-        });
-  }
-  
-
-  ngOnInit(): void {
-    // console.log(this._storage);
-    // this.vehicleService.getVehicles().subscribe(vehicles => {
-    //   console.log(vehicles);
-    // })
-    this._locationIqService.autoComplete("vastra").subscribe({
-      next: (res:any) => {
-        console.log(res);
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(value => {
+        if (value.trim().length === 0) {
+          this.fromSuggestions = [];
+        }
+      }),
+      filter(value => value.trim().length > 0),
+      switchMap(value => this._locationIqService.autoComplete(value))
+    ).subscribe({
+      next: (res: any) => {
+        this.fromSuggestions = res;
       },
       error: (err) => {
-        console.log("CompanyComponent ~ this._apiService.getCompany ~ err:", err)
+        console.error(err);
+        this.fromSuggestions = [];
       }
     });
 
+    this.toTextUpdate.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(value => {
+        if (value.trim().length === 0) {
+          this.toSuggestions = [];
+        }
+      }),
+      filter(value => value.trim().length > 0),
+      switchMap(value => this._locationIqService.autoComplete(value))
+    ).subscribe({
+      next: (res: any) => {
+        this.toSuggestions = res;
+      },
+      error: (err) => {
+        console.error(err);
+        this.toSuggestions = [];
+      }
+    });
   }
 
-  // async uploadFile():Promise<void>{
-  //   const storageRef = ref(this._storage,`Vehicles/${this.file.name}`);
-  //   const snap = await uploadBytes(storageRef,this.file);
-  //   this.fileName = `Vehicles/${this.file.name}`
-  // }
+  ngOnInit(): void {
+    // Any other initialization logic
+  }
+
+  selectFromSuggestion(suggestion: any) {
+    this.fromText = suggestion.display_name;
+    this.fromSuggestions = [];
+    this.fromLatLng = { lat: parseFloat(suggestion.lat), lon: parseFloat(suggestion.lon) };
+  }
+
+  selectToSuggestion(suggestion: any) {
+    this.toText = suggestion.display_name;
+    this.toSuggestions = [];
+    this.toLatLng = { lat: parseFloat(suggestion.lat), lon: parseFloat(suggestion.lon) };
+  }
 }
